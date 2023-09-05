81ff5de6174788ec438844735eb6f65b95acea78
GABRIEL BERNARDO MARTINS - SEMMA <T000007@pmc.local>
2023-09-05 16:30:21 -0300

    estrutura das paginas completa

---
+++ src/components/Head/index.tsx
@@ -0,0 +1,13 @@
+interface HeadProps {
+    title: string;
+    description?: string;
+}
+
+export function Head({ title, description = "" }: HeadProps) {
+
+    document.title = `Food Commerce | ${title}`
+    document.querySelector('[name="description"]')?.setAttribute('content', description);
+
+    return null
+
+}
